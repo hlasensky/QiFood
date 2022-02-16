005663a1bb314aacbf429c47c362c2a1d8ac802a
unknown <tomas.hlasensky@seznam.cz>
2022-02-16 08:25:38 +0100

    fixing auth middleware

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
     if (req.user.email === "anonym") {
+        return res.redirect('/login');
         next();
     } else if (!req.session.isLoggedIn || !req.session.isAdmin) {
         return res.redirect('/login');
