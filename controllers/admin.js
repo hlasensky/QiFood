d172de48ef2ba0fe82d6c971d65222dff62c924c
hlasensky <tomas.hlasensky@seznam.cz>
2022-03-01 19:43:46 +0100

    finnal fix

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,8 +8,7 @@ exports.getAddProduct = (req, res, next) => {
 	/* Rendering site for add product, plus passing categories */
 	Category.find()
 		.then((categories) => {
-			print(categories)
-			if (categories.length() > 0) {
+			if (categories.length !== 0) {
 				res.render("admin/edit-product", {
 					pageTitle: "Add Product",
 					path: "/admin/add-product",
